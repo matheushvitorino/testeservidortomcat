a72a79033abb8a17e247586ef72171483d194338
Matheus <matheushenriquevitorino@gmail.com>
2024-11-02 19:51:24 -0300

    first commit

---
+++ src/main/java/com/example/demo/TestedeserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestedeserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestedeserverApplication.class, args);
+	}
+
+}
